ff8698d0af360965af7fb771c5d3ad79f4609987
Kevillin Santos <kevillin_santos@hotmail.com>
2022-07-10 19:33:30 -0300

    Requisito 4 - GetRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,12 +1,22 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return id;
+  return employees.some((funcionario) => funcionario.managers.includes(id));
 }
 
-console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  if (isManager(managerId) === true) {
+    const confereGerente = employees.filter((e) => e.managers.some((i) => i === managerId));
+    const finalGerente = confereGerente
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+    return finalGerente;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
