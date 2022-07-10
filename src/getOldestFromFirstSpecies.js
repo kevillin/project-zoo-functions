ab9e356c080bef31b68ac9a435efaa77f8db7528
Kevillin Santos <kevillin_santos@hotmail.com>
2022-07-10 20:54:06 -0300

    Requisito 10 - getOldestFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.find((func) => func.id === id);
+  const animalResponsavel = funcionario.responsibleFor[0];
+  const animal = species.find((e) => e.id === animalResponsavel);
+
+  const achaAnimal = animal.residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  const resultado = Object.values(achaAnimal);
+
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
