576a119c0247359a81283b2776c993f6568a47cc
Kevillin Santos <kevillin_santos@hotmail.com>
2022-07-11 13:26:14 -0300

    Requisito 11 - getEmployeesCoverage. Finalmente. No limite

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function confereFunc({ name, id }) {
+  const func = employees.find((e) => e.id === id || e.firstName === name || e.lastName === name);
+  // confere se o parametro corresponde aos dados dos funcionarios
+  if (func === undefined) throw Error('Informações inválidas'); // lança um erro se não corresponder
+
+  const animais = species.filter((e) => func.responsibleFor.includes(e.id));
+  // verifica se o funcionario é responsavel pelo animal
+  return {
+    id: func.id,
+    fullName: `${func.firstName} ${func.lastName}`,
+    species: animais.map((especie) => especie.name),
+    locations: animais.map((especie) => especie.location),
+  };
+}
+
+function retornaTodos() {
+  return employees.map((employee) => confereFunc({ id: employee.id }));
+}
+
+function getEmployeesCoverage(objeto) {
+  if (objeto === undefined) return retornaTodos();
+  return confereFunc(objeto);
 }
 
 module.exports = getEmployeesCoverage;
